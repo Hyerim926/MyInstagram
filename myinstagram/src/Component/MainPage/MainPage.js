89c6b23cba82e36f55903ae4a9ca855e834f2932
Hyerim926 <ekfrhsk96@icloud.com>
2021-12-06 00:24:58 +0900

    storage 활성화

--- myinstagram/src/Component/MainPage/MainPage.js
+++ myinstagram/src/Component/MainPage/MainPage.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import Post from '../Post/Post';
 import "./MainPage.css";
+import { storage, auth } from "../firebase";
+
 import uploadImage from "../../images/images/more.png";
 
 class MainPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            postArray: []
+            postArray: [],
+            progressBar: "",
         }
     }
 
@@ -27,12 +30,44 @@ class MainPage extends Component {
         ];
         this.setState({ postArray: data });
     }
+
+    upload = (event) => {
+
+        let image = event.target.files[0];
+        const thisContext = this;
+        if (image === null || image === undefined)
+            return;
+
+        var uploadTask = storage.ref("images").child(image.name).put(image);
+        uploadTask.on('state_changed',
+            function (snapshot) {
+                var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                thisContext.setState({ progressBar: progress });
+            },
+            function (error) {
+            },
+            function () {
+                uploadTask.snapshot.ref.getDownloadURL().then(function (downloadURL) {
+                    console.log(downloadURL);
+                })
+            }
+        );
+    }
     render() {
         return (
             <div>
-                <div style={{ "textAlign": "center", "margin": "10px" }}>
-                    <img className="mainpage_uploadIcon" src={uploadImage} />
+                {/* <div style={{ "textAlign": "center", "margin": "10px" }}> */}
+                <div className="mainpage_container">
+                    <div className="mainpage_divider"></div>
+                    <div className="fileupload">
+                        <label for="file-upload">
+                            <img className="mainpage_uploadIcon" src={uploadImage} />
+                        </label>
+                        <input onChange={this.upload} id="file-upload" type="file" />
+                    </div>
+                    <div className="mainpage_divider"></div>
                 </div>
+                <div className="upload_text">{this.state.progressBar}</div>
                 {
                     this.state.postArray.map((item, index) => (
                         <Post id={item.postId} userName={item.userName} postImage={item.postImageURL} likes={item.likes} />
--- myinstagram/src/Component/firebase.js
+++ myinstagram/src/Component/firebase.js
@@ -1,17 +1,15 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import firebase from "firebase/compat";
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
+firebase.initializeApp({
     apiKey: "AIzaSyDtqIy3yc8WqKfO3XBBCvYgzyXVO_qIYQ8",
     authDomain: "myinstagram-80d5c.firebaseapp.com",
     projectId: "myinstagram-80d5c",
     storageBucket: "myinstagram-80d5c.appspot.com",
     messagingSenderId: "713891220884",
     appId: "1:713891220884:web:9c372ef8cfaa4d3276d381"
-};
+});
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const storage = firebase.storage();
+
+export { storage, auth };
